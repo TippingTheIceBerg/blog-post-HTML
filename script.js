1dc3200809d8b482d63746c41612adae51720d1c
OscarOrtiz <oortiz.cofo2014@gmail.com>
2022-06-13 13:35:48 -0500

    move js script to the bottom of the index, add JS to start on intersection observer

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let navBar = document.querySelector("#id")
-
-navBar.style.color = 'red';
-
-
+const image = document.querySelector(".image");
+const options = {rootMargin:"150px"};
+const observer = new IntersectionObserver(function(entries,observer){entries.forEach(entry => {
+        console.log(entry);
+    });
+},options);
+observer.observe(image);
