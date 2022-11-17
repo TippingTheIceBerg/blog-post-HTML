0adb8b3f243e43132c6572ef7b9a4b5eb494a036
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-11-17 00:59:43 -0600

    add backend section

---
+++ ruby.rb
@@ -0,0 +1,14 @@
+  # Multi-line
+  [1, 2, 3].each do |i|
+    # do stuff
+  end
+
+  # Does the same thing in single line
+  [1, 2, 3].each { |i| do_some_stuff }
+
+
+  class MyFirstClass
+  end
+
+  class MySecondClass
+  end
