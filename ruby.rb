a9d2fb9e9a4dfec1e3ed949ca5494c088553c50b
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-11-21 00:43:22 -0600

    add more ruby terminology

--- ruby.rb
+++ ruby.rb
@@ -1,14 +1,2 @@
-  # Multi-line
-  [1, 2, 3].each do |i|
-    # do stuff
-  end
-
-  # Does the same thing in single line
-  [1, 2, 3].each { |i| do_some_stuff }
-
-
-  class MyFirstClass
-  end
-
-  class MySecondClass
-  end
+print "bubuyu"
+puts "sadda"
