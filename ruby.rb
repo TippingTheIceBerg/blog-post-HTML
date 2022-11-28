f6b09e8261aeec4e490ee578f009fd6fba4695d6
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-11-28 00:38:51 -0600

    more ruby code

--- ruby.rb
+++ ruby.rb
@@ -7,3 +7,23 @@ puts "sadda"
 # To convert a float to an integer:
 13.0.to_i #=> 13
 13.9.to_i #=> 13
+
+# With the plus operator:
+"Welcome " + "to " + "Odin!"    #=> "Welcome to Odin!"
+
+# With the shovel operator:
+"Welcome " << "to " << "Odin!"  #=> "Welcome to Odin!"
+
+# With the concat method:
+"Welcome ".concat("to ").concat("Odin!")  #=> "Welcome to Odin!
+
+"hello"[0]      #=> "h"
+
+"hello"[0..1]   #=> "he"
+
+"hello"[0, 4]   #=> "hell"
+
+"hello"[-1]     #=> "o"
+
+print "2".to_i + 2
+
