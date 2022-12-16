5b98429a9b4d02a55ace296c25da697e20fe874e
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-12-16 00:55:29 -0600

    add more on case examples

--- ruby.rb
+++ ruby.rb
@@ -151,3 +151,34 @@ puts response #=> "You're all grown up."
 puts "x is 3" if x == 3
 puts "x is NOT 3" unless x == 3
 
+
+# accetpable ternary
+foo = hitchhiker ? 42 : 3.1415    // Assign result of ?: to a variable
+puts(hitchhiker ? 42 : 3.1415)    // Pass result as argument
+return hitchhiker ? 42: 3.1415    // Return result
+
+# case with only one puts
+a = 5
+
+answer = case a
+  when 5
+    "a is 5"
+  when 6
+    "a is 6"
+  else
+    "a is neither 5, nor 6"
+  end
+
+puts answer
+
+# case with no arguments
+a = 5
+
+case
+when a == 5
+  puts "a is 5"
+when a == 6
+  puts "a is 6"
+else
+  puts "a is neither 5, nor 6"
+end
