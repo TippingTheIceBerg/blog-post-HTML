6c2313183fd0257341c0760eb29f6f8252b8a557
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-03-07 14:51:59 -0600

    add section on node

--- quotes.js
+++ quotes.js
@@ -1,2 +1,2 @@
-quotes = ["Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.",
-"Is this entertaining?"]
+let quotes = ["Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.",
+"Is this entertaining?","Links and anchors defined by the A element must not be nested; an A element must not contain any other A elements.","Are you chasing your dream? Or have you fallen in love with just the chase. What happened to achieving it?"];
--- script.js
+++ script.js
@@ -1,3 +1,108 @@
+let quizHeadings = []
+let quizTexts = [];
+
+// get headings
+function getHeadings (){
+  let htmlContainer = document.querySelector(".html__container")
+  let htmlHeadings = htmlContainer.getElementsByTagNameNS("*","h3")
+  for(let a = 0; a <= htmlHeadings.length; a++){
+    quizHeadings.push(htmlHeadings[a])
+  }
+  
+  let cssContainer = document.querySelector(".css__container")
+  let cssHeadings = cssContainer.getElementsByTagNameNS("*","h3")
+  for(let b = 0; b <= cssHeadings.length; b++){
+    quizHeadings.push(cssHeadings[b])
+  }
+  
+  let jsContainer = document.querySelector(".js__container")
+  let jsHeadings = jsContainer.getElementsByTagNameNS("*","h3")
+  for(let c = 0; c <= jsHeadings.length; c++){
+    quizHeadings.push(jsHeadings[c])
+  }
+  
+  let gitContainer = document.querySelector(".git__container")
+  let gitHeadings = gitContainer.getElementsByTagNameNS("*","h3")
+  for(let d = 0; d <= gitHeadings.length; d++){
+    quizHeadings.push(gitHeadings[d])
+  }
+  
+  let vsContainer = document.querySelector(".vs__container")
+  let vsHeadings = vsContainer.getElementsByTagNameNS("*","h3")
+  for(let e = 0; e <= vsHeadings.length; e++){
+    quizHeadings.push(vsHeadings[e])
+  }
+  
+  let regexContainer = document.querySelector(".regex__container")
+  let regexHeadings = regexContainer.getElementsByTagNameNS("*","h3")
+  for(let f = 0; f <= regexHeadings.length; f++){
+    quizHeadings.push(regexHeadings[f])
+  }
+  
+  let backEndContainer = document.querySelector(".backEnd__container")
+  let backEndHeadings = backEndContainer.getElementsByTagNameNS("*","h3")
+  for(let g = 0; g <= backEndHeadings.length; g++){
+    quizHeadings.push(backEndHeadings[g])
+  }
+  
+    quizHeadings.forEach(heading =>{
+    console.log(quizHeadings)
+    }
+    )
+}
+// get text under headings
+
+function getTexts(){
+let htmlContainer = document.querySelector(".html__container")
+let htmlTexts = htmlContainer.getElementsByTagNameNS("*","p")
+for(let a = 0; a <= htmlTexts.length; a++){
+  quizTexts.push(htmlTexts[a])
+}
+
+let cssContainer = document.querySelector(".css__container")
+let cssTexts = cssContainer.getElementsByTagNameNS("*","p")
+for(let b = 0; b <= cssTexts.length; b++){
+  quizTexts.push(cssTexts[b])
+}
+
+let jsContainer = document.querySelector(".js__container")
+let jsText = jsContainer.getElementsByTagNameNS("*","p")
+for(let c = 0; c <= jsTexts.length; c++){
+  quizTexts.push(jsTexts[c])
+}
+
+let gitContainer = document.querySelector(".git__container")
+let gitTexts = gitContainer.getElementsByTagNameNS("*","p")
+for(let d = 0; d <= gitTexts.length; d++){
+  quizTexts.push(gitTexts[d])
+}
+
+let vsContainer = document.querySelector(".vs__container")
+let vsTexts = vsContainer.getElementsByTagNameNS("*","p")
+for(let e = 0; e <= vsTexts.length; e++){
+  quizTexts.push(vsTexts[e])
+}
+
+let regexContainer = document.querySelector(".regex__container")
+let regexTexts = regexContainer.getElementsByTagNameNS("*","p")
+for(let f = 0; f <= regexTexts.length; f++){
+  quizTexts.push(regexTexts[f])
+}
+
+let backEndContainer = document.querySelector(".backEnd__container")
+let backEndTexts = backEndContainer.getElementsByTagNameNS("*","p")
+for(let g = 0; g <= backEndTexts.length; g++){
+  quizTexts.push(backEndTexts[g])
+}
+
+  quizTexts.forEach(text =>{
+    console.log(text)
+  }
+  )
+}
+
+getHeadings();
+getTexts();
 
 let images = document.querySelectorAll(".images")
 let options = {threshold: .5};
@@ -87,6 +192,9 @@ entries.forEach(entry =>{
 backToTopInt.observe(nav);
 
 
+// Quiz
+// pull all h3 tags from one section
+// pull all p tags from one section
 
 
 
