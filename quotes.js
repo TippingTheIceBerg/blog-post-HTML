a230a2a9b16136760073e3a3a7f464cbd46f4353
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-05-20 15:40:15 -0500

    add advance CSS selectors

--- quotes.js
+++ quotes.js
@@ -1,2 +1,8 @@
-let quotes = ["Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.",
-"Is this entertaining?","Links and anchors defined by the A element must not be nested; an A element must not contain any other A elements.","Are you chasing your dream? Or have you fallen in love with just the chase. What happened to achieving it?"];
+let quotes = [
+  "Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.",
+  "Is this entertaining?",
+  "Links and anchors defined by the A element must not be nested; an A element must not contain any other A elements.",
+  "Are you chasing your dream? Or have you fallen in love with just the chase. What happened to achieving it?",
+  "1rem is the font-size of the root element (either :root or html). The math works the same with rem as it did with em, but without the added complexity of keeping track of the parent's font size. ",
+  "anything from 45 to 75 characters is widely regarded as a satisfactory length of line for a single-column page set in a serifed text face in a text size.",
+];
