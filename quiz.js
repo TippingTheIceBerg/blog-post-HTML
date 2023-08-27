07e3bf1eec39007458eed368dc9b1f3e1ee949a9
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-08-26 23:02:10 -0500

    add quiz card previous/next card

--- quiz.js
+++ quiz.js
@@ -1,4 +1,8 @@
-let headings = document.querySelector(".sections");
+let questions = ["how many eggs", "what is your shoe size"];
+let answer = ["2", "2 1/2"];
 
-window.localStorage.setItem('headings',`${headings}`);
-    
+let quiz__question = document.querySelector(".quiz__question");
+let quiz__answer = document.querySelector(".quiz__answer");
+
+let i = 0;
+let q = 0;
