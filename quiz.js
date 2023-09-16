108b854d7e434114c795b7f37518dd93fa0f11b1
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-09-15 22:00:32 -0500

    add numerical count of flashcard

--- quiz.js
+++ quiz.js
@@ -20,6 +20,15 @@ let htmlQuestions = [
   "If we are grouping sections of a form, what are the best ways to get this done?",
   "In what way can we label a fieldset with a 'title' to group like minded things",
   "when are articles used in html semantics?",
+  "What is the fastest way to open the terminal?",
+  "If you want to quickly select with a pointer dev tool, what's the best way?",
+  "What's the fastest way to open the console?",
+  "What is the BEM naming style?",
+  "What is the skeleton structure of BEM, IE, how will it look like?",
+  "What is important of lazy loading how is it done?",
+  "What can you do to allow links to open a new link when clicking on links?",
+  "How can we create a dropdown select?",
+  "What attribute will allow images to load later if the page is running slow? Hint, not lazy loading",
 ];
 
 let htmlAnswers = [
@@ -30,6 +39,15 @@ let htmlAnswers = [
   "To group forms, we can separate the form areas with either fieldset, divs, or even p's",
   "We can add a legend, if a legend tagname is included inside a fieldset, the section will be labeled with a heading and be given a border to separate it from other areas.",
   "Articles are stand alone sections. Think of blog posts, newspaper article,a comment or independent gadget",
+  "the terminal can be opened by hitting command + option + c",
+  "Quick pointer selection is to hit command + shift + c",
+  "Command + option + j, only works for chrome.",
+  "BEM is Block, element, and modifier",
+  "block__element--modifier, blocks are standalone, elements have no meaning without the block, and modifiers are changes. You can have a block without an element.",
+  "Lazy loading is an attribute you can add to the img and it's important to load times, you an add it  as a simple 'loading='lazy''.  ",
+  "add a target='_blank' to the link and a new tab will open ",
+  "We can create a dropdown with select and inside with option <select> <option> --Make a choice--</option> <option value='someValue'> </option> </select>",
+  "decoding:'async' will allow images to load later if the page is slow.",
 ];
 
 let cssQuestions = [
@@ -41,6 +59,9 @@ let cssQuestions = [
   "If we wanted to target invalid and valid forms, how do we select these?",
   "What is the way to remove bullet from a list?",
   "If we wanted to animate an 'idle' state of a list, how will we select this?",
+  "What's the best way to control and mess around with transforms and box-shadows?",
+  "For interesting shapes, what site can we use?",
+  "How do we center with position absolute ",
 ];
 
 let cssAnswers = [
@@ -52,6 +73,8 @@ let cssAnswers = [
   "we will use pseudocode input:valid or input:invalid",
   "remember to select either the class that aligns with li or select li directly and then style it as list-style:none.",
   "an idle state animation will require us to select input[type='radio']:indeterminate then add an animation to it, remember to set the keyframes for the animation as well.",
+  "transforms and box-shadows can be manipulated under the css elements under chrome. ",
+  "funky shapes can be made 'haikei' a site that can allow us to make lots of things.",
 ];
 
 question = htmlQuestions;
@@ -79,6 +102,11 @@ flip.addEventListener("click", () => {
   }
 });
 
+// number on deck
+let positionInDeck = document.querySelector(".cardNumber__current");
+
+let totalLengthOfDeck = document.querySelector(".cardNumber__total");
+
 // logic that allows it to go forward, if i is larger than the array, i is subtracted by one, which holds it's place in the array.
 forward.addEventListener("click", () => {
   i++;
@@ -87,9 +115,10 @@ forward.addEventListener("click", () => {
   }
   questionText.textContent = question[i];
   answerText.textContent = "";
+  getDeckPosition(i);
 });
 
-// if the array is smaller than 0, which can't happen due to the array index, it is halted.
+// if the array is smaller than 0, which can't happen due to the array index, it is halted, otherwise, shows the previous question and answers
 backwards.addEventListener("click", () => {
   i--;
   if (i < 0) {
@@ -98,6 +127,7 @@ backwards.addEventListener("click", () => {
   }
   questionText.textContent = question[i];
   answerText.textContent = "";
+  getDeckPosition(i);
 });
 
 // takes the question from the array and displays it
@@ -106,6 +136,7 @@ function showFirstQuestion(question) {
   i = 0;
   questionText.textContent = question[i];
   answerText.textContent = "";
+  totalLengthOfDeck.textContent = question.length;
 }
 
 showFirstQuestion(htmlQuestions);
@@ -128,6 +159,7 @@ selectHTML.addEventListener("click", () => {
   answer = htmlAnswers;
   selectHTML.classList.toggle("quiz__selection--active");
   showFirstQuestion(htmlQuestions);
+  getDeckPosition(0);
 });
 selectCSS.addEventListener("click", () => {
   removeActiveDeck();
@@ -135,7 +167,16 @@ selectCSS.addEventListener("click", () => {
   answer = cssAnswers;
   selectCSS.classList.toggle("quiz__selection--active");
   showFirstQuestion(cssQuestions);
+  getDeckPosition(0);
 });
+
+function getDeckLength(num) {
+  totalLengthOfDeck.value = num + 1;
+}
+
+function getDeckPosition(position) {
+  positionInDeck.value = position + 1;
+}
 
 // selectJS.addEventListener("click", () => {
 //   question = jsQuestions;
@@ -156,4 +197,4 @@ selectCSS.addEventListener("click", () => {
 //   question = regexQuestions;
 //   answer = regexAnswers;
 //   showFirstQuestion();
-// });
+// })
